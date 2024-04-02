113e77f454204e57762579af7888dd97f27656f0
kangseonghyeok <kangsh9820@naver.com>
2024-04-02 17:32:28 +0900

    [ENH] Vue Project Init

---
+++ src/components/KRA/KRA0101P01.vue
@@ -0,0 +1,23 @@
+<template>
+  <body>
+  <article>
+    <img src="@/assets/img/logo_kartrush.png" alt="카트라이더 러쉬플러스 매니지먼트 앱입니다.">
+
+    <div id="idInput">
+      <label for="userID">ID</label>
+      <input type="text" id="userId" name="userId" placeholder="ID를 입력하세요">
+    </div>
+    <div id="pwInput">
+      <label for="userPW">패스워드</label>
+      <input type="text" id="userPW" name="userPW" placeholder="패스워드를 입력하세요">
+    </div>
+
+    <button>로그인</button>
+  </article>
+  </body>
+</template>
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from "vue-router";
+import KRA0101P01 from "@/components/KRA/KRA0101P01.vue";
+
+const routes = [
+    {
+        path: '/',
+        component: KRA0101P01
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(), routes
+})
+
+export {router}
