540ad6c2e821e93b74a9747c0669f4f61a199963
kangseonghyeok <kangsh9820@naver.com>
2024-04-27 16:16:48 +0900

    [ENH] member Race add Page Add - 멤버 친선횟수 추가 페이지 작성

--- src/views/contents/AddNewRace.vue
+++ src/views/contents/AddNewRace.vue
@@ -13,7 +13,7 @@
         <tbody>
           <tr v-for="(itm, idx) in membersList" :key="idx">
             <td>
-              <input type="checkbox"/>
+              <input type="checkbox" :id="'chkbox' + itm.seq" @change="pageFunc.raceAdd(itm)" />
             </td>
             <td>{{ itm.nickname }}</td>
             <td>{{ itm.kartTierKo }}</td>
@@ -21,7 +21,7 @@
         </tbody>
       </table>
       <button id="loadMore" v-if="!isLast" @click="pageFunc.getMoreMemberList()">더보기</button>
-      <button id="addMore">횟수 추가</button>
+      <button id="addMore" @click="pageFunc.callRaceAddApi()">횟수 추가</button>
     </section>
   </body>
 </template>
@@ -30,6 +30,7 @@ import CommonHeader from "@/components/CommonHeader.vue";
 import {krmanage} from "@/plugins/krmanage.js";
 import {useRouter} from "vue-router";
 import {onMounted, ref} from "vue";
+import {useStore} from "vuex";
 
 export default {
   name: 'AddNewRace',
@@ -38,10 +39,15 @@ export default {
 
     const { $api, $ui, $utils } = krmanage()
     const router = useRouter();
+    const store = useStore();
+
+    const adminInfo = ref({})
 
     const membersList = ref([])
     const pageNumber = ref(0)
     const isLast = ref(false)
+
+    const raceAddList = ref([])
 
     const pageFunc = {
       getAllMemberList: () => {
@@ -51,7 +57,11 @@ export default {
           membersList.value = res.response.content
           res.response.last === true ? isLast.value = true : isLast.value = false
         }, err => {
-          console.log(err)
+          $ui.alert({
+            title: "네트워크 오류",
+            content: "세션이 만료되었거나 없습니다. 다시 로그인 바랍니다."
+          });
+          router.push("/")
         })
       },
       getMoreMemberList: () => {
@@ -70,10 +80,63 @@ export default {
           });
           router.push("/")
         })
-      }
+      },
+      raceAdd: itm => {
+        if ($utils.isEmpty(raceAddList.value.find(listItm => listItm === itm))) {
+          raceAddList.value.push(itm);
+        } else {
+          let findIndex = raceAddList.value.findIndex(listItm => listItm === itm);
+          raceAddList.value.splice(findIndex, 1)
+        }
+        console.log(raceAddList.value)
+      },
+      callRaceAddApi: () => {
+        if ($utils.isEmpty(raceAddList.value)) {
+          $ui.alert({
+            title: "실패",
+            content: "선택된 멤버가 없습니다."
+          });
+          return false
+        }
+
+        let params = {
+          groupSeq: adminInfo.value.groupSeq,
+          members: raceAddList.value
+        }
+
+        $api(`api/race/add`, params, 'post', res => {
+          console.log(res)
+          if (res.code === '000') {
+            $ui.alert({
+              title: "성공",
+              content: res.message
+            });
+            raceAddList.value.forEach(itm => {
+              document.querySelector(`#chkbox${itm.seq}`).checked = false
+            })
+            raceAddList.value = []
+          } else {
+            $ui.alert({
+              title: "실패",
+              content: res.message
+            });
+            raceAddList.value.forEach(itm => {
+              document.querySelector(`#chkbox${itm.seq}`).checked = false
+            })
+            raceAddList.value = []
+          }
+        },err => {
+          $ui.alert({
+            title: "네트워크 오류",
+            content: "세션이 만료되었거나 없습니다. 다시 로그인 바랍니다."
+          });
+          router.push("/")
+        })
+      },
     }
 
     onMounted(() => {
+      adminInfo.value = store.getters['getMemberInfo']
       pageFunc.getAllMemberList();
     });
 
